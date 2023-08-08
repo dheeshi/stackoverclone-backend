c0567767477ef2734b5ff91e64c74505dcf0bebe
dheeshith <dheeshithmeloth@gmail.com>
2023-08-08 19:33:41 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv "
+import dotenv from "dotenv";
 
 import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
